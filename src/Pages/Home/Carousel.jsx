55deee89115b9e81c66bf559cda84ec44d93622b
Dandokku <jesulobadaniel1@gmail.com>
2023-10-19 10:43:55 +0100

    A few Ui adjustments

--- src/Pages/Home/Carousel.jsx
+++ src/Pages/Home/Carousel.jsx
@@ -23,7 +23,7 @@ function Carousel() {
                 <SwiperSlide className='relative'>
                     <img src={Image1} className='w-full h-[95vh] '/>
                     <div className='absolute inset-0 p-20 flex gap-7 justify-center items-center flex-col w-full h-full z-[9999] text-secondaryColor bg-linearBackground'>
-                        <h1 className='textshadow text-5xl sm:text-xl font-bold text-whiteColor text-shadow-md text-center'> <span className='text-secondaryColor'>Book</span> Your Laundry Services <br /> Hassle Free</h1>
+                        <h1 className='textshadow text-5xl font-bold text-whiteColor text-shadow-md text-center'> <span className='text-secondaryColor'>Book</span> Your Laundry Services <br /> Hassle Free</h1>
                         <p className='text-whiteColor text-center'>We offer affordable pricing options so that you can spend more time doing the things you love,<br></br> and less time worrying about laundry</p>
                         <Link to='./login' className='btnbtn py-3 px-10 bg-whiteColor text-textColor hover:bg-textColor hover:text-whiteColor rounded-md'>Book Now</Link>
                     </div>
