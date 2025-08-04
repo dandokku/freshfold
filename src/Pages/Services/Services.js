26917a23229992d03c1ff57fa566373bcad3ba26
Dandokku <jesulobadaniel1@gmail.com>
2025-08-04 08:58:50 +0100

    serviceses

--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,14 +1,11 @@
 import React from 'react'
 import OurFeatures from './OurFeatures'
-import Discount from './Discount'
 import OfferedServices from './OfferedServices'
 
 function Services() {
   return (
     <div className='pt-14'>
       <OfferedServices />
-      <OurFeatures />
-      <Discount />
     </div>
   )
 }
